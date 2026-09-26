6bdbfb4ea87e3ae9e16ccee5dbe540daa52d8c70
Waeel Mohammed <wael.mohammed@dtc.iq>
2026-09-26 19:26:20 +0300

    update

--- Frontend/react/my-react/src/App.jsx
+++ Frontend/react/my-react/src/App.jsx
@@ -18,46 +18,102 @@ function Card({ title, footer, children }) {
   );
 }
 
-function ProductCard({ product: { name, price, inStock }, onBuy, onRemove }) {
-
+function ProductCard({ product: { id, name, price, stock }, onBuy, onRemove }) {
   return (
     <Card
       title={name}
       footer={
         <>
-          <Button onClick={() => onBuy(name)} disabled={!inStock}>
-            {inStock ? "شراء" : "نفِد"}
+          <Button onClick={() => onBuy(id)} disabled={stock <= 0}>
+            {stock > 0 ? "شراء" : "نفِد"}
           </Button>
-          <Button variant="danger" onClick={() => onRemove(name)}>
+          <Button variant="danger" onClick={() => onRemove(id)}>
             حذف
           </Button>
         </>
       }
     >
       <p>السعر: {price}</p>
-      <p>المخزون: {inStock ? "متوفر" : "غير متوفر"}</p>
+      <p>المخزون: {stock > 0 ? stock : "غير متوفر"}</p>
     </Card>
   );
 }
 
 export default function App() {
-  const products = [
-    { name: "Product 1", price: 100, inStock: true },
-    { name: "Product 2", price: 200, inStock: false },
-    { name: "Product 3", price: 300, inStock: true },
+  const allProducts = [
+    { id: 1, name: "Product 1", price: 100, stock: 10 },
+    { id: 2, name: "Product 2", price: 200, stock: 0 },
+    { id: 3, name: "Product 3", price: 300, stock: 10 },
   ];
+  const [products, setProducts] = useState(allProducts);
 
-  const handleRemove = (productName) => {
-    alert(`${productName} removed successfully`);
-    products.splice(products.indexOf(productName), 1);
+  const handleRemove = (productId) => {
+    alert(`${productId} removed successfully`);
+    const newProducts = products.filter((product) => product.id !== productId);
+    setProducts(newProducts);
   };
 
-  const handleBuy = (productName) => {
-    alert(`${productName} bought successfully`);
+  const handleBuy = (productId) => {
+    alert(`${productId} bought successfully`);
+    const newStock = products.map((product) =>
+      product.id === productId
+        ? { ...product, stock: product.stock - 1 }
+        : product,
+    );
+    setProducts(newStock);
+  };
+
+  const handleAscending = () => {
+    const sortedProducts = products.toSorted((a, b) => a.price - b.price);
+    setProducts(sortedProducts);
+  };
+
+  const handleDescending = () => {
+    const sortedProducts = products.toSorted((a, b) => b.price - a.price);
+    setProducts(sortedProducts);
+  };
+
+  const handleShowOnlyAvailable = (checked) => {
+    if (checked) {
+      const filteredProducts = products.filter((product) => product.stock > 0);
+      setProducts(filteredProducts);
+    } else {
+      setProducts(allProducts);
+    }
   };
 
   return (
     <>
+      <h1>Products</h1>
+      <div
+        style={{
+          display: "flex",
+          gap: "10px",
+          justifyContent: "center",
+          marginBottom: "10px",
+        }}
+      >
+        <Button onClick={() => handleDescending}>
+          ترتيب حسب السعر : تنازلي
+        </Button>
+        <Button onClick={() => handleAscending}>
+          ترتيب حسب السعر : تصاعدي
+        </Button>
+      </div>
+      <div
+        style={{
+          display: "flex",
+          gap: "10px",
+          justifyContent: "center",
+          marginBottom: "10px",
+        }}
+      >
+        <p>اظهار المتاح فقط :</p>
+        <input
+          type="checkbox"
+          onChange={(e) => handleShowOnlyAvailable(e.target.checked)}
+        />
+      </div>
       {products.map((product) => (
         <ProductCard
           key={product.name}
